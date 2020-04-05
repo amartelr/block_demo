453f840bf0e2211cbe243d8f4fdc8792219b4b5f
amartelr <alfredo.martel@gmail.com>
2020-04-05 12:45:10 +0200

    Añadimos la lógica de negocio

--- lib/block/block.dart
+++ lib/block/block.dart
@@ -1,8 +1,8 @@
 import 'package:rxdart/rxdart.dart';
 import 'package:meta/meta.dart';
 
-
 abstract class Bloc<Event, State> {
+  // _eventSubject = StreamController
   final PublishSubject<Event> _eventSubject = PublishSubject<Event>();
   BehaviorSubject<State> _stateSubject;
 
@@ -12,15 +12,26 @@ abstract class Bloc<Event, State> {
 
   Bloc() {
     _stateSubject = BehaviorSubject<State>.seeded(initialState);
+    _bindStateSubject();
   }
 
   @mustCallSuper
   void dispose() {
-    _eventSubject.close();  
+    _eventSubject.close();
     _stateSubject.close();
   }
 
   void dispatch(Event event) {
     _eventSubject.sink.add(event);
+  }
+
+  Stream<State> mapEventToState(Event event);
+
+  void _bindStateSubject() {
+    _eventSubject.asyncExpand(mapEventToState).forEach(
+      (State nextState) {
+        _stateSubject.sink.add(nextState);
+      },
+    );
   }
 }
