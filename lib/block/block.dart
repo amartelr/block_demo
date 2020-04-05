5c3b8823aff8d0233dfe8705538563e5ad44a21d
amartelr <alfredo.martel@gmail.com>
2020-04-05 12:29:33 +0200

    Método de Cierre

--- lib/block/block.dart
+++ lib/block/block.dart
@@ -1,23 +1,23 @@
 import 'package:rxdart/rxdart.dart';
 
-
 abstract class Bloc<Event, State> {
   final PublishSubject<Event> _eventSubject = PublishSubject<Event>();
   BehaviorSubject<State> _stateSubject;
-
 
   State get initialState;
   State get currentState => _stateSubject.value;
   Stream<State> get state => _stateSubject.stream;
 
-
-    Bloc() {
-    _stateSubject = BehaviorSubject<State>.seeded(initialState);    
+  Bloc() {
+    _stateSubject = BehaviorSubject<State>.seeded(initialState);
   }
 
+  void dispose() {
+    _eventSubject.close();
+    _stateSubject.close();
+  }
 
   void dispatch(Event event) {
     _eventSubject.sink.add(event);
   }
-    
 }
