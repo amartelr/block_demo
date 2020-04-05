502616d0d280060ebe5ebfa1de6e4d9ac44a1449
amartelr <alfredo.martel@gmail.com>
2020-04-05 12:21:31 +0200

    Creamos la Salida del BLoC

--- lib/block/block.dart
+++ lib/block/block.dart
@@ -3,6 +3,18 @@ import 'package:rxdart/rxdart.dart';
 
 abstract class Bloc<Event, State> {
   final PublishSubject<Event> _eventSubject = PublishSubject<Event>();
+  BehaviorSubject<State> _stateSubject;
+
+
+  State get initialState;
+  State get currentState => _stateSubject.value;
+  Stream<State> get state => _stateSubject.stream;
+
+
+    Bloc() {
+    _stateSubject = BehaviorSubject<State>.seeded(initialState);    
+  }
+
 
   void dispatch(Event event) {
     _eventSubject.sink.add(event);
