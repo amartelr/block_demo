b840f3146e73810a151a9fd191e12c174dd27fef
amartelr <alfredo.martel@gmail.com>
2020-04-05 12:15:34 +0200

    Creamos las Entradas del BLoC

--- lib/block/block.dart
+++ lib/block/block.dart
@@ -2,5 +2,10 @@ import 'package:rxdart/rxdart.dart';
 
 
 abstract class Bloc<Event, State> {
+  final PublishSubject<Event> _eventSubject = PublishSubject<Event>();
 
+  void dispatch(Event event) {
+    _eventSubject.sink.add(event);
+  }
+    
 }
